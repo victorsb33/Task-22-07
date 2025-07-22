63f94c6e194a0488f9c1426e3ccc071f9b2cb171
Victor Stock <victorstock33@gmail.com>
2025-07-21 23:50:07 -0300

    incremntando if/else

---
+++ scrit.js
@@ -0,0 +1,28 @@
+    // Fazer uma função para manipular informações de uma barberia.
+
+    /*if, else if, else
+    /*variávis
+    /*console.log
+    /*lista
+    /*for
+    */
+
+    // Lista para armazenar informações do cliente
+    var Agendamentos = [];
+
+      // Variáveis
+      let nome = document.getElementById("nome").value;
+      let telefone = document.getElementById("telefone").value;
+      let servico = document.getElementById("serviço").value;
+      let horario = document.getElementById("horario").value;
+        
+      // if/else
+      if (nome === "" || telefone=== "" || servico === "" || horario === "") {
+        alert("Preencha este campo.");
+      } else {
+       
+        };
+
+        
+      
+    
